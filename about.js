fbd01fdcddd81302da3a902cdc0b1f678693b9e0
Walter Taylor <walterdctaylor@yahoo.com>
2022-08-12 12:41:13 -0600

    test

---
+++ about.js
@@ -0,0 +1,16 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert("Form has been submitted successfully")
+}
+
+
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
+
+element.addEventListener("mouseover"Be Happy),
